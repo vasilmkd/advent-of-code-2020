a8a7a4a2edd54e08331e174515f2c74a6559c680
Vasil Vasilev <vasilv@protonmail.com>
2020-12-02 20:21:29 +0100

    Day 2

---
+++ src/main/scala/day2.scala
@@ -0,0 +1,34 @@
+import scala.io.Source
+
+case class Config(letter: Char, min: Int, max: Int)
+
+def parseConfig(cfg: String): Config =
+  val parts = cfg.split(" ")
+  val letter = parts(1)(0)
+  val minMax = parts(0).split("-")
+  val min = minMax(0).toInt
+  val max = minMax(1).toInt
+  Config(letter, min, max)
+
+def parse(line: String): (Config, String) =
+  val parts = line.split(": ")
+  (parseConfig(parts(0)), parts(1))
+
+def validate1(config: Config, password: String): Boolean =
+  val cnt = password.count(_ == config.letter)
+  cnt >= config.min && cnt <= config.max
+
+def validate2(config: Config, password: String): Boolean =
+  val fst = password(config.min - 1) == config.letter
+  val snd = password(config.max - 1) == config.letter
+  fst ^ snd
+
+@main
+def day2part1(): Unit =
+  val result = Source.fromResource("day2.txt").getLines().count(parse andThen validate1)
+  println(result)
+
+@main
+def day2part2(): Unit =
+  val result = Source.fromResource("day2.txt").getLines().count(parse andThen validate2)
+  println(result)
