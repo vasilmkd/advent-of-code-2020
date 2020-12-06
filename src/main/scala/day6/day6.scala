10b663998371d0f1a9f767d1435753e00b38605a
Vasil Vasilev <vasilv@protonmail.com>
2020-12-06 23:59:15 +0100

    Day 6

---
+++ src/main/scala/day6/day6.scala
@@ -0,0 +1,44 @@
+package day6
+
+import scala.io.Source
+
+val Universe = "abcdefghijklmnopqrstuvwxyz".toSet
+
+trait Monoid[A]:
+  def identity: A
+  extension (x: A) def combine(y: A): A
+
+case class State(sum: Int, partial: Set[Char])
+
+def foldFn(using monoid: Monoid[Set[Char]])(state: State, line: String): State =
+  if line.isEmpty then
+    val count = state.partial.size
+    State(state.sum + count, monoid.identity)
+  else
+    State(state.sum, state.partial.combine(line.toSet))
+
+@main
+def part1(): Unit =
+  given Monoid[Set[Char]]:
+    def identity: Set[Char] = Set.empty
+    extension (x: Set[Char]) def combine(y: Set[Char]): Set[Char] =
+      x.union(y)
+
+  val result =
+    (Source.fromResource("day6.txt").getLines() ++ List(""))
+      .foldLeft(State(0, summon[Monoid[Set[Char]]].identity))(foldFn)
+      .sum
+  println(result)
+
+@main
+def part2(): Unit =
+  given Monoid[Set[Char]]:
+    def identity: Set[Char] = Universe
+    extension (x: Set[Char]) def combine(y: Set[Char]): Set[Char] =
+      x.intersect(y)
+
+  val result =
+    (Source.fromResource("day6.txt").getLines() ++ List(""))
+      .foldLeft(State(0, summon[Monoid[Set[Char]]].identity))(foldFn)
+      .sum
+  println(result)
